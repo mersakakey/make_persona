a0fa49a0ed086e52fd03b5a2946bec65ef5a6850
mersakakey <ca02098@cartahd.com>
2023-07-14 14:04:24 +0900

    [fix]エラーハンドリングを追加

--- make_csvfile.py
+++ make_csvfile.py
@@ -28,11 +28,19 @@ def make_csvfile(persona_str_list):
 
         persona_name, persona_value = text_to_list(i)
 
+        
         print(persona_name,persona_value)
+
+        if (persona_name == [] or persoma_value == []):
+            continue
 
         if name == "":
             name = persona_name
         value_list.append(persona_value)
+    
+    if value_list == []:
+        print("error")
+        return 0
 
     df = pd.DataFrame(data = value_list, columns = name)
 
@@ -40,6 +48,17 @@ def make_csvfile(persona_str_list):
 
     return csv
 
-    
-#make_csvfile(["[名前:] アレックス [性別:] 男性 [年齢:] 22歳 [国籍:] 日本 [住所:] 東京都 [学歴:] 大学卒業 [職業:] エンジニア [役職:] 新卒 [収入:] 時給制 [既婚、未婚:] 未婚 [家族構成:] 両親と2人の兄弟 [友人の数:] 10人","[名前:] アレックス [性別:] 男性 [年齢:] 22歳 [国籍:] 日本 [住所:] 東京都 [学歴:] 大学卒業 [職業:] エンジニア [役職:] 新卒 [収入:] 時給制 [既婚、未婚:] 未婚 [家族構成:] 両親と2人の兄弟 [友人の数:] 10人"])
 
+# make_csvfile(["""名前:高田健一
+# 性別:男性
+# 年齢:22歳
+# 国籍:日本
+# 住所:東京都渋谷区
+# 学歴:名古屋大学 工学部情報工学科卒業
+# 職業:エンジニア（AI系）
+# 趣味:プログラミング、アウトドア活動、スポーツ観戦
+# 特技:アルゴリズム設計、言語学習
+# 収入:500万
+# 既婚、未婚:未婚
+# 家族構成:父、母、姉（25歳）
+# """])
